6310abcb17b06ee1a2f091cdbad9b128343128ae
monyatto <atsuuu.0925@gmail.com>
2023-01-29 10:53:11 +0900

    ご指摘いただいた箇所を修正

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -4,7 +4,6 @@ class BlogsController < ApplicationController
   skip_before_action :authenticate_user!, only: %i[index show]
 
   before_action :set_blog, only: %i[show edit update destroy]
-  before_action :correct_user, only: %i[edit update destroy]
 
   def index
     @blogs = Blog.search(params[:term]).published.default_order
@@ -21,7 +20,6 @@ class BlogsController < ApplicationController
   def create
     @blog = current_user.blogs.new(blog_params)
     if @blog.save
-      @blog.update(random_eyecatch: false) if @blog.random_eyecatch && current_user.premium == false
       redirect_to blog_url(@blog), notice: 'Blog was successfully created.'
     else
       render :new, status: :unprocessable_entity
@@ -30,7 +28,6 @@ class BlogsController < ApplicationController
 
   def update
     if @blog.update(blog_params)
-      @blog.update(random_eyecatch: false) if @blog.random_eyecatch && current_user.premium == false
       redirect_to blog_url(@blog), notice: 'Blog was successfully updated.'
     else
       render :edit, status: :unprocessable_entity
@@ -38,7 +35,6 @@ class BlogsController < ApplicationController
   end
 
   def destroy
-    correct_user
     @blog.destroy!
 
     redirect_to blogs_url, notice: 'Blog was successfully destroyed.', status: :see_other
@@ -47,22 +43,19 @@ class BlogsController < ApplicationController
   private
 
   def set_blog
-    if Blog.find(params[:id]).secret
-      if user_signed_in?
-        correct_user
+    @blog =
+      if Blog.find(params[:id]).secret
+        user_signed_in? ? current_user.blogs.find(params[:id]) : Blog.where(secret: false).find(params[:id])
       else
-        @blog = Blog.where(secret: false).find(params[:id])
+        action_name == 'show' ? Blog.find(params[:id]) : current_user.blogs.find(params[:id])
       end
-    else
-      @blog = Blog.find(params[:id])
-    end
   end
 
   def blog_params
-    params.require(:blog).permit(:title, :content, :secret, :random_eyecatch)
-  end
-
-  def correct_user
-    @blog = current_user.blogs.find(params[:id])
+    if current_user.premium
+      params.require(:blog).permit(:title, :content, :secret, :random_eyecatch)
+    else
+      params.require(:blog).permit(:title, :content, :secret)
+    end
   end
 end
